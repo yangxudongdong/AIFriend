2f17b6605ded2eefd3207e9353ffb76994c074bb
xudong yang <206336183@qq.com>
2026-03-27 20:08:03 +0800

    删除角色时删除对应的角色头像和背景图片

--- backend/web/views/create/character/remove.py
+++ backend/web/views/create/character/remove.py
@@ -1,8 +1,11 @@
+from os import remove
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 from web.models.character import Character
+from web.views.utils.photo import remove_old_photo
 
 
 class RemoveCharacterView(APIView):
@@ -10,7 +13,10 @@ class RemoveCharacterView(APIView):
     def post(self, request):
         try:
             character_id = request.data['character_id']
-            Character.objects.fileter(pk=character_id, author__user=request.user).delete()
+            character = Character.objects.fileter(pk=character_id, author__user=request.user)
+            remove_old_photo(character.photo)
+            remove_old_photo(character.background_image)
+            character.delete()
             return Response({
                 'result': 'success',
             })
