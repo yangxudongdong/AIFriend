eb58d0dce1171f249020c3e584d869b18efce898
xudong yang <206336183@qq.com>
2026-03-28 16:49:50 +0800

    实现搜索功能

--- backend/web/views/homepage/index.py
+++ backend/web/views/homepage/index.py
@@ -2,13 +2,20 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from web.models.character import Character
-
+from django.db.models import Q
 
 class HomepageIndexView(APIView):
     def get(self, request):
         try:
             items_count = int(request.query_params.get('items_count', 0))
-            characters_raw = Character.objects.all().order_by('-id')[items_count: items_count + 20]
+            search_query = request.query_params.get('search_query', '').strip()
+            if search_query:
+                queryset = Character.objects.filter(
+                    Q(name__icontains=search_query) | Q(profile__icontains=search_query)
+                )
+            else:
+                queryset = Character.objects.all()
+            characters_raw = queryset.order_by('-id')[items_count: items_count + 20]
             characters = []
             for character in characters_raw:
                 author = character.author
