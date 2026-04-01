1d0e014c9c45cc250b33a2cba297c84bc427e0e8
xudong yang <206336183@qq.com>
2026-04-01 18:03:30 +0800

    添加长期记忆

--- backend/web/views/friend/message/chat/chat.py
+++ backend/web/views/friend/message/chat/chat.py
@@ -10,6 +10,8 @@ from rest_framework.permissions import IsAuthenticated
 
 from web.models.friend import Friend, Message, SystemPrompt
 from web.views.friend.message.chat.graph import ChatGraph
+from web.views.friend.message.memory.update import update_memory
+
 
 class SSERenderer(BaseRenderer):
     media_type = 'text/event-stream'
@@ -24,6 +26,7 @@ def add_system_prompt(state,friend):
     for sp in system_prompts:
         prompt += sp.prompt
     prompt += f'\n【角色性格】\n{friend.character.profile}\n'
+    prompt += f'【长期记忆】\n{friend.memory}\n'
     return {'messages': [SystemMessage(prompt)] + msgs}
 
 def add_recent_messages(state, friend):
@@ -86,6 +89,8 @@ class MessageChatView(APIView):
                 output_tokens=output_tokens,
                 total_tokens=total_tokens,
             )
+            if Message.objects.filter(friend=friend).count() % 1 == 0:
+                update_memory(friend)
 
         response = StreamingHttpResponse(event_stream(), content_type='text/event-stream')
         response['Cache-Control'] = 'no-cache'
